dd1a18cf49ffe49900ed91e8f659a7a2a5a18636
Amar Kaygude <115354555+amar019@users.noreply.github.com>
2025-01-07 12:29:35 +0530

    Update RightTriangle.java

--- RightTriangle.java
+++ RightTriangle.java
@@ -3,7 +3,11 @@ public class RightTriangle {
 
     public static void main(String[] args)
     {
-        for(int i=1;i<=4;i++)
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no.of.rows :");
+        int n=sc.nextInt();
+        
+        for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=i;j++)
             {
